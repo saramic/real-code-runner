1147ceccfa4ed5c90af84eb4c3cd1deed70462b0
Michael Milewski <saramic@gmail.com>
2019-12-15 16:44:33 +1100

    credentaials.secret_key_base to use the ones in credentials.yml.enc
    not sure how secrets.secret_key_base relates?

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,6 @@ class User < ApplicationRecord
     JWT.encode({
       id: id,
       exp: 60.days.from_now.to_i,
-    }, Rails.application.secrets.secret_key_base)
+    }, Rails.application.credentials.secret_key_base )
   end
 end
