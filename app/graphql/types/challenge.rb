77888c6e83eba66c1e0bf7ce0c331377800ebb03
Michael Milewski <saramic@gmail.com>
2020-01-13 19:32:17 +1100

    return status for challenge
    uploaded or processed

--- app/graphql/types/challenge.rb
+++ app/graphql/types/challenge.rb
@@ -3,6 +3,7 @@ module Types
     field :id, ID, null: false
     field :title, String, null: false
     field :description, String, null: true
+    field :status, String, null: true
     field :metadata, Types::ChallengeMetadata, null: true
     field :features, [Types::Feature], null: true
     field :feature_file_urls, [Types::Url], null: true
